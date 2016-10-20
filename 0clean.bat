dfc9f02607cf3d39cff5c51b312849f5e0ab1bd2
houxd <houxd_f@foxmail.com>
2016-10-20 10:06:25 +0800

    首次提交

--- 0clean.bat
+++ 0clean.bat
@@ -8,4 +8,8 @@ del /Q fix_name_v2\fix_name\*.bin
 del /Q fix_name_v2\Debug\*
 del /Q fix_name_v2\fix_name\Debug\*
 
+del /Q jlink_dcc_view\Debug\*
+del /Q jlink_dcc_view\jlink_dcc\Debug\*
+
+
 pause
