7ab92c05fd1b62edf539b54a061226a1f1dc4e3f
houxd <houxd_f@foxmail.com>
2016-10-20 10:01:38 +0800

    首次整理

---
+++ 0clean.bat
@@ -0,0 +1,11 @@
+del /Q fix_name\*.bin
+del /Q fix_name\fix_name\*.bin
+del /Q fix_name\Debug\*
+del /Q fix_name\fix_name\Debug\*
+
+del /Q fix_name_v2\*.bin
+del /Q fix_name_v2\fix_name\*.bin
+del /Q fix_name_v2\Debug\*
+del /Q fix_name_v2\fix_name\Debug\*
+
+pause
